f8c6bfe4b69537f9ecab176783f2c7bf36addb32
Deepthi Gunasekara <DGunasekara@users.noreply.github.com>
2024-11-12 14:19:58 +0530

    Update LetsEncrypt_Script.sh

--- LetsEncrypt_Script.sh
+++ LetsEncrypt_Script.sh
@@ -7,4 +7,4 @@ working_dir=$the_install_base/shibboleth-idp5-installer/repository
 
 cd $working_dir || exit
 
-ansible-playbook -i ansible_hosts site_v4_LetsEncrypt.yml --force-handlers --extra-var="install_base=$the_install_base"
+ansible-playbook -i ansible_hosts site_v5_LetsEncrypt.yml --force-handlers --extra-var="install_base=$the_install_base"
